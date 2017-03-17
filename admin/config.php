3aacdc5c448e205dc4b7bac3c65c7a05a079d634
maryjane <maryjanepascua89>
2017-03-17 15:05:33 +0800

    Change database name.

--- admin/config.php
+++ admin/config.php
@@ -3,7 +3,7 @@
 	$DB_HOST = 'localhost';
 	$DB_USER = 'root';
 	$DB_PASS = '';
-	$DB_NAME = 'our_upou';
+	$DB_NAME = 'our';
 	
 	try{
 		$DB_con = new PDO("mysql:host={$DB_HOST};dbname={$DB_NAME}",$DB_USER,$DB_PASS);
