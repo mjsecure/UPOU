cc200c56869a7af1977248d4f1971132fed4fbf0
maryjane <maryjanepascua89>
2017-03-17 15:19:18 +0800

    Add database port number in admin/config.php

--- admin/config.php
+++ admin/config.php
@@ -4,7 +4,8 @@
 	$DB_USER = 'root';
 	$DB_PASS = '';
 	$DB_NAME = 'our';
-	
+	$DB_PORT = 1234;
+
 	try{
 		$DB_con = new PDO("mysql:host={$DB_HOST};dbname={$DB_NAME}",$DB_USER,$DB_PASS);
 		$DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
