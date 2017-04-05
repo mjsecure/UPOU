3da176499dfe649aff0a18136fca24eb7121fdf0
maryjane <maryjanepascua89>
2017-04-05 17:11:29 +0800

    Initial Commit

--- admin/addnew_user.php
+++ admin/addnew_user.php
@@ -35,9 +35,7 @@ if(isset($_POST['btn-signup']))
 	else if(strlen($upass) < 6){
 		$error[] = "Password must be atleast 6 characters";	
 	}
-  else if(strlen($upass) > 10){
-    $error[] = "Password must be atleast 10 characters"; 
-  }
+ 
 
 	else
 	{
